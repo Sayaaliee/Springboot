aefda8219fe1f3ee1953f6b7d4942454802d4d5e
Sayaaliee <164638650+Sayaaliee@users.noreply.github.com>
2024-05-29 22:25:47 +0530

    Update SpringbootDemoApplication.java

--- src/main/java/com/star/agile/assignment/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/star/agile/assignment/springbootdemo/SpringbootDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hiiii This my Jenkins Assignment No.1");
 		SpringApplication.run(SpringbootDemoApplication.class, args);
 	}
 
