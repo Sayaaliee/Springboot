3d100e202f9a7900cb02beb37f7f9f6e3bb66ad6
Sayaaliee <ksayali619@gmail.com>
2024-06-01 12:39:16 +0530

    Hello word change

--- src/main/java/com/star/agile/assignment/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/star/agile/assignment/springbootdemo/SpringbootDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootDemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hiiii This my Jenkins Assignment No.1");
+		System.out.println("Hello This my Jenkins Assignment No.1");
 		SpringApplication.run(SpringbootDemoApplication.class, args);
 	}
 
